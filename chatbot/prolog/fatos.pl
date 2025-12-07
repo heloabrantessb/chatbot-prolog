7c0805c02f1d771c03e18b38b3ee9bd969b9fbda
heloabrantessb <heloisa.abrantesdev@gmail.com>
2025-12-06 23:51:43 -0300

    feat: criar arquivo fatos.pl para determinar uma base de fatos do sistema

---
+++ chatbot/prolog/fatos.pl
@@ -0,0 +1,38 @@
+% Bruxos
+personagem(harry potter, grifinoria).
+personagem(hermione granger, grifinoria).
+personagem(rony weasley, grifinoria).
+personagem(draco malfoy, sonserina).
+personagem(luna lovegood, corvinal).
+personagem(cedrico diggory, lufalufa).
+
+% Patronos
+patrono(harry, 'Cervo').
+patrono(hermione, 'Lontra').
+patrono(rony, 'Cão Terrier').
+patrono(luna, 'Lebre').
+patrono(cedrico, 'Desconhecido').
+patrono(draco, 'Desconhecido').
+
+% Descrições das casas
+casa(grifinoria, 'Coragem e bravura').
+casa(sonserina, 'Ambição e astúcia').
+casa(lufalufa, 'Lealdade e trabalho duro').
+casa(corvinal, 'Sabedoria e criatividade').
+
+% Chefe de casa
+chefe_de_casa(grifinoria, 'Minerva McGonagall').
+chefe_de_casa(sonserina, 'Severus Snape').
+chefe_de_casa(lufalufa, 'Pomona Sprout').
+chefe_de_casa(corvinal, 'Filius Flitwick').
+
+% Feitiços e suas descrições
+feitico(expelliarmus, 'Feitiço de desarmamento').
+feitico(lumos, 'Cria luz na ponta da varinha').
+feitico(expecto_patronum, 'Invoca um Patrono contra dementadores').
+feitico(wingardium_leviosa, 'Faz objetos flutuarem').
+
+% Respostas diretas
+resposta(ola, 'Olá! Como posso ajudar?').
+resposta(hogwarts, 'Hogwarts é uma escola de magia e bruxaria').
+resposta(despedida, 'Até logo!!').
---
+++ chatbot/prolog/regras.pl
@@ -0,0 +1,1 @@
+:- consult('fatos.pl').
